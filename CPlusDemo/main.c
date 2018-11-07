ba3d31793b5e4e209cd3bf964f9f5707d10a8ee3
ybjia <ybjia@mobvoi.com>
2018-11-07 15:48:33 +0800

    Initial Commit

---
+++ CPlusDemo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CPlusDemo
+//
+//  Created by xibei sunny on 2018/11/7.
+//  Copyright © 2018 xibei sunny. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
