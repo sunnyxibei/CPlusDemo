9dbad8872beeda78032de78dff48604d26dfa643
ybjia <ybjia@mobvoi.com>
2018-12-17 10:49:03 +0800

    Re-Format code

--- CPlusDemo/main.c
+++ CPlusDemo/main.c
@@ -23,16 +23,16 @@
 void bubbleSort(int arr[] , int length);
 
 int main(int argc, const char * argv[]) {
-//    printf("测试冒泡排序\n");
-//    int arr[] = {2,3,1,21132,2,32,245,12};
-//    int length = sizeof(arr)/sizeof(arr[1]);
-//    bubbleSort(arr, length);
+    //    printf("测试冒泡排序\n");
+    //    int arr[] = {2,3,1,21132,2,32,245,12};
+    //    int length = sizeof(arr)/sizeof(arr[1]);
+    //    bubbleSort(arr, length);
     
-//    printf("测试指针运算\n");
-//    printf("第一个例子，字符串拷贝\n");
-//    char dst[100];
-//    strCopy("Hello World !",dst);
-//    printf("%s\n",dst);
+    //    printf("测试指针运算\n");
+    //    printf("第一个例子，字符串拷贝\n");
+    //    char dst[100];
+    //    strCopy("Hello World !",dst);
+    //    printf("%s\n",dst);
     
     testArray();
 }
@@ -64,7 +64,7 @@ int main01(int argc, const char * argv[]) {
     system("cd /usr/include/;ls -l | grep stdio.h");
     system("python --version");
     printf("%d\n",MAX);
-   
+    
     short x = 0;
     int y = 1;
     long z = 2L;
@@ -85,94 +85,94 @@ int main01(int argc, const char * argv[]) {
     //打印变量的内存地址
     printf("mem(x) = %p\n",&x);
     
-//    int a;
-//    printf("please input a integer number \n");
-//    scanf("%d",&a);
-//    switch (a) {
-//        case 1:
-//            printf("星期一\n");
-//            break;
-//        case 2:
-//            printf("星期二\n");
-//            break;
-//        case 3:
-//            printf("星期三\n");
-//            break;
-//        case 4:
-//            printf("星期四\n");
-//            break;
-//        case 5:
-//            printf("星期五\n");
-//            break;
-//        case 6:
-//            printf("星期六\n");
-//            break;
-//        default:
-//            printf("星期天\n");
-//            break;
-//    }
-//
+    //    int a;
+    //    printf("please input a integer number \n");
+    //    scanf("%d",&a);
+    //    switch (a) {
+    //        case 1:
+    //            printf("星期一\n");
+    //            break;
+    //        case 2:
+    //            printf("星期二\n");
+    //            break;
+    //        case 3:
+    //            printf("星期三\n");
+    //            break;
+    //        case 4:
+    //            printf("星期四\n");
+    //            break;
+    //        case 5:
+    //            printf("星期五\n");
+    //            break;
+    //        case 6:
+    //            printf("星期六\n");
+    //            break;
+    //        default:
+    //            printf("星期天\n");
+    //            break;
+    //    }
+    //
     //一维数组初始化的方式
     //全局数组若不初始化，默认初始化为零 局部l数组若不初始化，默认初始化为随机值
     
-//    demo 一维数组的反转
-//    int arr[] = { 1, -2, 3,- 4, 5, -6, 7, -8, -9, 10 };
-//    int length = sizeof(arr)/sizeof(arr[0]);
-//    printf("原始数组为:\n");
-//    for(int x = 0; x < length; x++ ){
-//        printf("arr[%d] = %d\n",x,arr[x]);
-//    }
-//
-//    //两个标记位i和j，分别从两端开始遍历
-//    int i = 0;
-//    int j = length -1;
-//    while(i < j){
-//        //交换两个位置上的数组元素
-//        int temp = arr[i];
-//        arr[i] = arr[j];
-//        arr[j] = temp;
-//        i++;
-//        j--;
-//    }
-//
-//    printf("反转数组为:\n");
-//    for(int y = 0; y < length; y++ ){\
-//        printf("arr[%d] = %d\n",y,arr[y]);
-//    }
-//
-//    printf("arr = %p\n",arr);
+    //    demo 一维数组的反转
+    //    int arr[] = { 1, -2, 3,- 4, 5, -6, 7, -8, -9, 10 };
+    //    int length = sizeof(arr)/sizeof(arr[0]);
+    //    printf("原始数组为:\n");
+    //    for(int x = 0; x < length; x++ ){
+    //        printf("arr[%d] = %d\n",x,arr[x]);
+    //    }
+    //
+    //    //两个标记位i和j，分别从两端开始遍历
+    //    int i = 0;
+    //    int j = length -1;
+    //    while(i < j){
+    //        //交换两个位置上的数组元素
+    //        int temp = arr[i];
+    //        arr[i] = arr[j];
+    //        arr[j] = temp;
+    //        i++;
+    //        j--;
+    //    }
+    //
+    //    printf("反转数组为:\n");
+    //    for(int y = 0; y < length; y++ ){\
+    //        printf("arr[%d] = %d\n",y,arr[y]);
+    //    }
+    //
+    //    printf("arr = %p\n",arr);
     
-   // bubbleSort(&arr);
-
+    // bubbleSort(&arr);
     
-//    //定义了一个二维数组，名字叫a
-//    //二维数组是本质上还是一维数组，此一维数组有3个元素
-//    //每个元素又是一个一维数组int[4]
-//    int a[3][4] = { 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12  };
-//
-//    //数组名为数组首元素地址，二维数组的第0个元素为一维数组
-//    //第0个一维数组的数组名为a[0]
-//    printf("a = %p\n", a);
-//    printf("a[0] = %p\n", a[0]);
-
-//    char str1[] = {'a',' ','b','c'};
-//    printf("%s\n",str1);
-//
-//    char str2[] = {'a',' ','b','c','\0'};
-//    printf("%s\n",str2);
-//
-//    //字符串处理以‘\0’(ASCII码值=0)作为结束符，后面的'h', 'l', 'l', 'e', 'o'不会输出
-//    char c3[] = { 'c', ' ', 'p', 'r', 'o', 'g', '\0', 'h', 'l', 'l', 'e', 'o', '\0'};
-//    printf("c3 = %s\n", c3);
     
-//    //不指定长度, 没有0结束符，有多少个元素就有多长
-//    char buf[] = { 'a', 'b', 'c' };
-//    printf("buf = %s\n", buf);    //乱码
-//
-//    //指定长度，后面没有赋值的元素，自动补0
-//    char buf2[100] = { 'a', 'b', 'c' };
-//
-//    printf("buf2 = %s\n", buf2);
+    //    //定义了一个二维数组，名字叫a
+    //    //二维数组是本质上还是一维数组，此一维数组有3个元素
+    //    //每个元素又是一个一维数组int[4]
+    //    int a[3][4] = { 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12  };
+    //
+    //    //数组名为数组首元素地址，二维数组的第0个元素为一维数组
+    //    //第0个一维数组的数组名为a[0]
+    //    printf("a = %p\n", a);
+    //    printf("a[0] = %p\n", a[0]);
+    
+    //    char str1[] = {'a',' ','b','c'};
+    //    printf("%s\n",str1);
+    //
+    //    char str2[] = {'a',' ','b','c','\0'};
+    //    printf("%s\n",str2);
+    //
+    //    //字符串处理以‘\0’(ASCII码值=0)作为结束符，后面的'h', 'l', 'l', 'e', 'o'不会输出
+    //    char c3[] = { 'c', ' ', 'p', 'r', 'o', 'g', '\0', 'h', 'l', 'l', 'e', 'o', '\0'};
+    //    printf("c3 = %s\n", c3);
+    
+    //    //不指定长度, 没有0结束符，有多少个元素就有多长
+    //    char buf[] = { 'a', 'b', 'c' };
+    //    printf("buf = %s\n", buf);    //乱码
+    //
+    //    //指定长度，后面没有赋值的元素，自动补0
+    //    char buf2[100] = { 'a', 'b', 'c' };
+    //
+    //    printf("buf2 = %s\n", buf2);
     
     char * arr = "abc\0defg";
     printf("%s\n", arr);
@@ -184,26 +184,26 @@ int main01(int argc, const char * argv[]) {
     time(&mill);
     printf("%ld\n",mill);
     
-//    int a;
-//    int b;
-//    int c;
-// __asm用于执行汇编指令
-//    __asm
-//    {
-//        mov a, 3    //3的值放在a对应内存的位置
-//        mov b, 4    //4的值放在b对应内存的位置
-//        mov eax, a    //把a内存的值放在eax寄存器
-//        add eax, b    //eax和b相加，结果放在eax
-//        mov c, eax    //eax的值放在c中
-//    }
-//
-//    printf("%d\n",c);
+    //    int a;
+    //    int b;
+    //    int c;
+    // __asm用于执行汇编指令
+    //    __asm
+    //    {
+    //        mov a, 3    //3的值放在a对应内存的位置
+    //        mov b, 4    //4的值放在b对应内存的位置
+    //        mov eax, a    //把a内存的值放在eax寄存器
+    //        add eax, b    //eax和b相加，结果放在eax
+    //        mov c, eax    //eax的值放在c中
+    //    }
+    //
+    //    printf("%d\n",c);
     
-   // printRandom();
+    // printRandom();
     testPointer();
-//    testPointerWithArray();
-//    testPointerOperator();
-
+    //    testPointerWithArray();
+    //    testPointerOperator();
+    
     return 0;
 }
 
--- CPlusDemo/testarray.c
+++ CPlusDemo/testarray.c
@@ -9,41 +9,42 @@
 #include <string.h>
 #include "testarray.h"
 
-void newStrcat(char* src,char* amend);
+void newStrcat(char* src, char* amend);
 
 void removeSpaceViaPointer(char* src);
 
-void removeSpace(char* src,long length);
+void removeSpace(char* src, long length);
 
-void testArray(){
-    int a[] = {1,2,3};
-    int b[] = {4,5,6};
-    int c[] = {7,8,9};
+void testArray() {
     
-    int* arr[] = {a,b,c};
+    int a[] = {1, 2, 3};
+    int b[] = {4, 5, 6};
+    int c[] = {7, 8, 9};
+    
+    int* arr[] = {a, b, c};
     //数组arr的首地址
-    printf("%p\n",arr);
+    printf("%p\n", arr);
     //数组arr第一个元素的首地址，这里其实还是一个二级指针
-    printf("%p\n",&arr[0]);
+    printf("%p\n", &arr[0]);
     //数组arr第一个元素的值的首地址
-    printf("%p\n",a);
+    printf("%p\n", a);
     
     //指针数组是一个特殊的二维数组
     //下面测试一下多级指针的操作
     int** p = arr;
     //遍历并打印指针数组
-    for (int i = 0; i < 3; i ++) {
+    for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            printf("%d， ",arr[i][j]);
+            printf("%d， ", arr[i][j]);
         }
         puts("");
     }
     
-    for (int i = 0; i < 3; i ++) {
+    for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             //二级指针的偏移，代表的是数组的行数的移动
             //一级指针的偏移，代表的是列数（具体的元素）的移动
-            printf("%d， ",*(*(arr + i) + j));
+            printf("%d， ", *(*(arr + i) + j));
         }
         puts("");
     }
@@ -57,43 +58,43 @@ void testArray(){
     int*** p3 = &p2;
     
     //这里牢记一句话，*是升维操作，&是降维操作
-    printf("x的值为: %d\n",x);
-    printf("x的值为: %d\n",*p1);
-    printf("x的值为: %d\n",**p2);
-    printf("x的值为: %d\n",***p3);
+    printf("x的值为: %d\n", x);
+    printf("x的值为: %d\n", *p1);
+    printf("x的值为: %d\n", **p2);
+    printf("x的值为: %d\n", ***p3);
     
     //这里尝试使用多级指针更改变量的值
     //所有的问题的根源，就是这次CPU的运算，操作的是到底是哪块内存区域
-    //p2里面存储的其实是p1的地址，把p2的值改了，也就是说，把p1的指向改了
-    //p1中存储的，变成了y的地址
+    // p2里面存储的其实是p1的地址，把p2的值改了，也就是说，把p1的指向改了
+    // p1中存储的，变成了y的地址
     *p2 = &y;
-    printf("x的值为: %d\n",***p3);
+    printf("x的值为: %d\n", ***p3);
     
     //这里和上面的方式是等价的，因为 *p2 == p1
     p1 = &x;
-    printf("x的值为: %d\n",***p3);
+    printf("x的值为: %d\n", ***p3);
     
     //数组名作为参数传递时，会丢失数组的精度，所以要把数组的精度（长度|维度）作为参数传递到函数中
     
     char ch[] = "hello";
-    printf("%lu\n",sizeof(ch));
-    printf("%lu\n",strlen(ch));
+    printf("%lu\n", sizeof(ch));
+    printf("%lu\n", strlen(ch));
     
     printf("测试字符串的追加");
     char src[100] = "hello";
     char amend[] = "world";
-    newStrcat(src,amend);
-    printf("追加之后的字符串结果：%s\n",src);
+    newStrcat(src, amend);
+    printf("追加之后的字符串结果：%s\n", src);
     
     printf("测试字符串的去除空格操作\n");
     char hello[] = "   h e l  l    o    w  or l   d ! ";
-
-    removeSpace(hello,strlen(hello));
+    
+    removeSpace(hello, strlen(hello));
     removeSpaceViaPointer(hello);
 }
 
 /*使用指针偏移操作字符串*/
-void newStrcat(char* src,char* amend){
+void newStrcat(char* src, char* amend) {
     while (*src) {
         src++;
     }
@@ -120,7 +121,7 @@ void removeSpaceViaPointer(char* src){
 void removeSpace(char* src,long length){
     int i = 0;
     int j = 0;
-   
+    
     for (i = 0 ; i < length; i ++) {
         if (src[i] != ' ') {
             src[j] = src[i];
@@ -131,4 +132,3 @@ void removeSpace(char* src,long length){
     src[j] = 0;
     printf("%s\n",src);
 }
-
